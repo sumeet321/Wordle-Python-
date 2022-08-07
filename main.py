3475cc3187912a2eba591f9d30fbca3f969d782b
Sumeet Singh <69872102+sumeet321@users.noreply.github.com>
2022-08-07 22:18:00 +0000

    Fixed bugs

--- main.py
+++ main.py
@@ -1,82 +1,49 @@
 import random
-
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N','O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 file = open("wordList.txt", "r")
 wordList = file.readlines()
-word = wordList[random.randint(0, len(wordList) - 1)].upper()
+word = wordList[random.randint(0, len(wordList) - 1)].upper() # makes all the words uppercase
+
+print(word)
 
 print("Welcome to Wordle!")
-i = 1
-while i < 7:
-    print()
-    print(i)
+tries = 1
+while tries < 7:
+	print()
+	print("Try", tries)
+	print(alphabet)
+	choice = input("\nChoose a 5-letter word: ").upper()
+	tries = tries + 1
 
-    choice = input("Choose a 5-letter word: ").upper()
-    i = i + 1
+	if word[0] == choice[0] and word[1] == choice[1] and word[2] == choice[2] and word[3] == choice[3] and word[4] == choice[4]:
+		print("You guessed the word: " + word)
+		break
+		
+	else:
+		if len(choice) == 5:    
+			for i in range(5):
+				if choice[i] == word[i]:
+					print(word[i] + " is correct.")
 
-    if len(choice) == 5:
-      if (choice[0] != word[0] and choice[0] != word[1]
-      and choice[0] != word[2] and choice[0] != word[3]
-      and choice[0] != word[4] and choice[1] != word[0]
-      and choice[1] != word[1] and choice[1] != word[2]
-      and choice[1] != word[3] and choice[1] != word[4]
-      and choice[2] != word[0] and choice[2] != word[1]
-      and choice[2] != word[2] and choice[2] != word[3]
-      and choice[2] != word[4] and choice[3] != word[0]
-      and choice[3] != word[1] and choice[3] != word[2]
-      and choice[3] != word[3] and choice[3] != word[4]
-      and choice[4] != word[0] and choice[4] != word[1]
-      and choice[4] != word[2] and choice[4] != word[3]
-      and choice[4] != word[4]):
-        print("There is no letter in the word.")
+				# if the letter is in the word but not in that place
+				if choice[i] in word and choice[i] != word[i]:
+					print(choice[i] + " is in the word but not in that place")
+					
+				if choice[i] not in word:
+					if choice[i] not in alphabet: # have to put this if statement because it will give an error if it tries to remove a letter from the alphabet list that is already taken out.
+						print(choice[i], "is not in the word")
+					else:
+						print(choice[i], "is not in the word.")
+						alphabet.remove(choice[i])
 
-      if (choice[0] == word[0] and choice[1] == word[1]
-      and choice[2] == word[2] and choice[3] == word[3]
-      and choice[4] == word[4]):
-        print("You guessed the word: " + word)
-        quit()
+		else:
+			print("The letter must be 5 letters! Please try again.")
+			tries = tries - 1
+			continue
 
-      if (choice[0] == word[0]):
-        print(word[0] + " is correct.")
+	if tries == 7:
+		print("\nYou ran out of tries! The word was " + word) # prints once while loop is done
 
-      if (choice[1] == word[1]):
-        print(choice[1] + " is correct.")
 
-      if (choice[2] == word[2]):
-        print(choice[2] + " is correct.")
-
-      if (choice[3] == word[3]):
-        print(choice[3] + " is correct.")
-
-      if (choice[4] == word[4]):
-        print(choice[4] + " is correct.")
-
-      if ((choice[0] != word[0])
-      and (choice[0] == word[1] or choice[0] == word[2]
-      or choice[0] == word[3] or choice[0] == word[4])):
-        print(choice[0] + " is in the word but not in that place.")
-
-      if ((choice[1] != word[1])
-      and (choice[1] == word[0] or choice[1] == word[2]
-      or choice[1] == word[3] or choice[1] == word[4])):
-        print(choice[1] + " is in the word but not in that place.")
-
-      if ((choice[2] != word[2])
-      and (choice[2] == word[0] or choice[2] == word[1]
-      or choice[2] == word[3] or choice[2] == word[4])):
-        print(choice[2] + " is in the word but not in that place.")
-
-      if ((choice[3] != word[3])
-      and (choice[3] == word[0] or choice[3] == word[1]
-      or choice[3] == word[2] or choice[3] == word[4])):
-        print(choice[3] + " is in the word but not in that place.")
-
-      if ((choice[4] != word[4]) and (choice[4] == word[0] or choice[4] == word[1] or choice[4] ==         word[2] or choice[4] == word[3])):
-        print(choice[4] + " is in the word but not in that place.")
-
-    else:
-      print("The letter must be 5 letters! Please try again.")
-      i = i - 1
-      continue
-
-    if i == 7:
-      print("\nYou ran out of tries! The word was " + word)
+# for loop is used when we are comparing each letter in one iteration.
+# for loop is not used when all the letters have to be compared with each other like guessing the entire word
